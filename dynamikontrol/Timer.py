b88b09e5be875a2ba502db2f1504170da83c5a0b
Brad <kairess87@gmail.com>
2021-02-19 17:35:28 +0900

    add timer class

---
+++ dynamikontrol/Timer.py
@@ -0,0 +1,58 @@
+import time
+from datetime import datetime, timezone
+import threading
+
+class Timer(object):
+    __stop_thread = False
+
+    def __init__(self):
+        pass
+
+    def callback_after(self, func, args=(), kwargs={}, after=0, interval=None):
+        self.__stop_thread = False
+        if interval is None:
+            self.__stop_thread = True
+
+        def handler():
+            time.sleep(after)
+            func(*args, **kwargs)
+
+            next_call = time.time()
+            while True:
+                if self.__stop_thread:
+                    break
+
+                next_call = next_call + interval
+                time.sleep(next_call - time.time())
+                func(*args, **kwargs)
+
+        timer_thread = threading.Thread(target=handler)
+        timer_thread.start()
+
+    def callback_at(self, func, args=(), kwargs={}, at=None, interval=None):
+        delta = datetime.strptime(at, '%Y-%m-%d %H:%M:%S') - datetime.now()
+        after = delta.seconds
+
+        if after < 0:
+            return False
+
+        self.callback_after(func=func, args=args, kwargs=kwargs, after=after, interval=interval)
+
+    def stop(self):
+        self.__stop_thread = True
+
+if __name__ == '__main__':
+    t1 = Timer()
+
+    def print_time(a, b):
+        print(a, b, datetime.now())
+
+    t1.callback_at(func=print_time, args=('AAAAAA',), kwargs={'b': 'bb'}, at='2021-02-19 17:16:30', interval=0.1)
+
+    t1.callback_after(func=print_time, args=('aa',), kwargs={'b': 'bb'}, after=2, interval=0.1)
+
+    time.sleep(5)
+
+    t1.stop()
+
+
