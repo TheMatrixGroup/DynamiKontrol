f035ce186dc0ff952412a870f857e3930f9882ee
Brad <kairess87@gmail.com>
2021-02-25 16:55:55 +0900

    remove serial hard codes

--- dynamikontrol/Module.py
+++ dynamikontrol/Module.py
@@ -5,36 +5,54 @@ import threading
 import time
 
 class Module(object):
+    serial_no = None
     port = None
     ser = None
     baud = 9600
     vid = None
     pid = None
+    # TODO: our vids and pids
+    avail_vids = ['0403']
+    avail_pids = ['6001']
+
     is_connected = False
 
-    serial_receive_delay = 0.01
+    __serial_receive_delay = 0.01
     __stop_thread = False
 
     def __init__(self, serial_no=None):
+        self.serial_no = serial_no
+
         ports = list_ports.comports(include_links=True)
 
         # TODO: filter dk port and assign to port
-        # TODO: filter by vid, pid
         for port in ports:
             desc = port[2].lower()
             if 'vid:pid' not in desc:
                 continue
 
             start_idx = desc.find('vid:pid=')
-            self.vid, self.pid = port[2][start_idx+8:start_idx+8+9].split(':')
+            self.vid, self.pid = desc[start_idx+8:start_idx+8+9].split(':')
 
-        # TODO: delete hard code
-        self.port = 'tty_dk_rec'
+            if self.vid not in self.avail_vids or self.pid not in self.avail_pids:
+                continue
+
+            start_idx = desc.find('ser=')
+            serial_no_str = desc[start_idx+4:start_idx+4+8]
+
+            if self.serial_no is not None and self.serial_no != serial_no_str:
+                continue
+
+            self.port = port[0]
+            break
 
         self.connect()
 
 
     def connect(self):
+        if self.port is None:
+            raise IOError('Module is not connected!')
+
         self.ser = serial.Serial(self.port, self.baud, timeout=0)
 
         self.receive_thread = threading.Thread(target=self.receive, args=())
@@ -58,7 +76,7 @@ class Module(object):
                 data = self.ser.readline().decode()
                 if data:
                     self.handle_data(data)
-                time.sleep(self.serial_receive_delay)
+                time.sleep(self.__serial_receive_delay)
 
         self.is_connected = False
         self.__stop_thread = False
@@ -69,15 +87,16 @@ class Module(object):
         if self.ser is None:
             raise IOError('Serial is not connected!')
 
-        # TODO: remove test serial port
-        ser = serial.Serial('tty_dk_sen', self.baud, timeout=0)
+        if self.port is None:
+            raise IOError('Module is not connected!')
+
+        ser = serial.Serial(self.port, self.baud, timeout=0)
         ser.write(data.encode())
 
 
 if __name__ == '__main__':
     m = Module()
 
-    print(m.__stop_thread)
     m.send('Hello world!')
 
     time.sleep(5)
