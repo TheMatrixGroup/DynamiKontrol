396ba43de8bf1d29ddc4111ef1cb1bb298603776
Brad <kairess87@gmail.com>
2021-03-04 14:47:36 +0900

    led

--- dynamikontrol/LED.py
+++ dynamikontrol/LED.py
@@ -1,19 +1,71 @@
 class LED(object):
+    type = 0x01
+    command = {
+        'r': 0x00,
+        'y': 0x01,
+        'g': 0x02
+    }
+    mode = {
+        'off': 0x00,
+        'on': 0x01,
+        'toggle': 0x02,
+        'blink': 0x03
+    }
+
     def __init__(self, module):
         self.m = module
 
-    # TODO: all led on
-    def on(self, id):
-        data = self.m.p2m.set_command(0x03).set_data([0x01, 0x01, 0x01]).encode()
+
+    def __send(self, color, mode, on_interval=0, off_interval=0):
+        on_low_bit = on_interval & 0xFF
+        on_high_bit = (on_interval >> 8) & 0xFF
+
+        off_low_bit = off_interval & 0xFF
+        off_high_bit = (off_interval >> 8) & 0xFF
+
+        data = self.m.p2m.set_type(self.type).set_command(self.command[color]).set_data([self.mode[mode], on_high_bit, on_low_bit, off_high_bit, off_low_bit]).encode()
         self.m.send(data)
 
 
-    # TODO: all led off
-    def off(self, id):
-        data = self.m.p2m.set_command(0x03).set_data([0x00, 0x00, 0x00]).encode()
-        self.m.send(data)
+    def off(self, color='all'):
+        if color in ['all', 'r']:
+            self.__send('r', 'off')
 
-    # TODO: all led toggle
-    def toggle(self, id):
-        data = self.m.p2m.set_command(0x03).set_data([0x02, 0x02, 0x02]).encode()
-        self.m.send(data)
+        if color in ['all', 'y']:
+            self.__send('y', 'off')
+
+        if color in ['all', 'g']:
+            self.__send('g', 'off')
+
+
+    def on(self, color='all'):
+        if color in ['all', 'r']:
+            self.__send('r', 'on')
+
+        if color in ['all', 'y']:
+            self.__send('y', 'on')
+
+        if color in ['all', 'g']:
+            self.__send('g', 'on')
+
+
+    def toggle(self, color='all'):
+        if color in ['all', 'r']:
+            self.__send('r', 'toggle')
+
+        if color in ['all', 'y']:
+            self.__send('y', 'toggle')
+
+        if color in ['all', 'g']:
+            self.__send('g', 'toggle')
+
+
+    def blink(self, color='all', on_interval=256, off_interval=256):
+        if color in ['all', 'r']:
+            self.__send('r', 'blink', on_interval, off_interval)
+
+        if color in ['all', 'y']:
+            self.__send('y', 'blink', on_interval, off_interval)
+
+        if color in ['all', 'g']:
+            self.__send('g', 'blink', on_interval, off_interval)
--- tests/led.py
+++ tests/led.py
@@ -6,16 +6,67 @@ import time
 
 module = Module(debug=True)
 
+# All on - off
 start_time = time.time()
-
 while True:
     if time.time() - start_time > 3:
         break
 
-    module.led.on(id=0)
+    module.led.on(color='all')
     time.sleep(0.1)
 
-    module.led.off(id=0)
+    module.led.off(color='all')
     time.sleep(0.1)
+
+# Red on - off
+start_time = time.time()
+while True:
+    if time.time() - start_time > 3:
+        break
+
+    module.led.on(color='r')
+    time.sleep(0.1)
+
+    module.led.off(color='r')
+    time.sleep(0.1)
+
+# Yellow on - off
+start_time = time.time()
+while True:
+    if time.time() - start_time > 3:
+        break
+
+    module.led.on(color='y')
+    time.sleep(0.1)
+
+    module.led.off(color='y')
+    time.sleep(0.1)
+
+# Green on - off
+start_time = time.time()
+while True:
+    if time.time() - start_time > 3:
+        break
+
+    module.led.on(color='g')
+    time.sleep(0.1)
+
+    module.led.off(color='g')
+    time.sleep(0.1)
+
+# Toggle
+start_time = time.time()
+while True:
+    if time.time() - start_time > 3:
+        break
+
+    module.led.toggle(color='all')
+    time.sleep(0.1)
+
+# Blink
+module.led.blink(color='r', on_interval=1000, off_interval=100)
+module.led.blink(color='y', on_interval=1000, off_interval=100)
+module.led.blink(color='g', on_interval=1000, off_interval=100)
+time.sleep(3)
 
 module.disconnect()
