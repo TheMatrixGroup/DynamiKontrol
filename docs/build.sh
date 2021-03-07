00e25928605fe32d23c852894e997da2402e5583
Brad <kairess87@gmail.com>
2021-03-07 17:55:41 +0900

    add korean docs

--- docs/build.sh
+++ docs/build.sh
@@ -2,3 +2,8 @@
 rm -rf build/*
 sphinx-apidoc -f -o source/ ../dynamikontrol ../dynamikontrol/Protocol.py ../dynamikontrol/helpers
 make html
+
+cd source
+sphinx-build -b gettext ./ ../build/gettext
+sphinx-intl update -p ../build/gettext -l ko_KR
+sphinx-build -b html -D language=ko_KR ./ ../build/html/ko_KR
