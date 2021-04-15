79384235d1b46817a62111bc52c7a59e16ad44ff
Brad <kairess87@gmail.com>
2021-04-15 17:18:50 +0900

    0.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setuptools.setup(
     name="DynamiKontrol",
-    version="0.1.2",
+    version="0.2.0",
     author="Taehee Lee",
     author_email="kairess87@gmail.com",
     description="DynamiKontrol Python API",
